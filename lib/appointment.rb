14f131bd17efa62ffcee56f16605ddd5c9e33b9b
Shannon Young <shannon.ari1@gmail.com>
2021-06-15 18:46:01 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,16 +1,16 @@
-class Appointment 
+class Appointment
   @@all = []
   attr_accessor :date, :patient, :doctor
 
   def initialize(date, patient, doctor)
-    @date = date 
-    @patient = patient 
-    @doctor = doctor 
-    @@all << self 
-  end 
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
 
   def self.all
-    @@all 
-  end 
+    @@all
+  end
 
 end
